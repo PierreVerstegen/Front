ed29c3e9d560b63a101caa3b2ba0bb520984c418
Pierre Verstegen <pierrev21@gmail.com>
2025-11-20 14:56:50 +0100

    Added signals to brig charac create service

--- Projet/src/component/shared/forms/brig-form-component/brig-form-component.ts
+++ Projet/src/component/shared/forms/brig-form-component/brig-form-component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
-
+import { Component, inject, NgModule } from '@angular/core';
+import { combineLatestAll } from 'rxjs';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { BrigCharacCreateService } from '../../../../core/service/brig-charac-create-service/brig-charac-create-service';
 @Component({
   selector: 'app-brig-form-component',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './brig-form-component.html',
   styleUrl: './brig-form-component.css',
 })
 export class BrigFormComponent {
 
-charac_race : string = ""
+// Signaux pour variables locales
+characCreateService = inject(BrigCharacCreateService)
+charac_race = this.characCreateService.charac_race
+stats = this.characCreateService.stats
+
+// Méthodes appelant service
+
+humainStats(){
+this.characCreateService.humainStats
+console.log(this.stats)
+console.log(this.charac_race)
+}elfeStats(){
+this.characCreateService.elfeStats
+}
+nainStats(){
+this.characCreateService.nainStats
+}
+halfelinStats(){
+this.characCreateService.halfelinStats
+}
+
+
+
+
 
 }
--- Projet/src/core/service/brig-charac-create-service/brig-charac-create-service.ts
+++ Projet/src/core/service/brig-charac-create-service/brig-charac-create-service.ts
@@ -1,10 +1,25 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BrigCharacCreateService {
-  
+  charac_race = signal<string>("");
+  stats = signal<{ [key: string]: number }>({
+  combat: 0,
+  connaissances: 0,
+  discretion: 0,
+  endurance: 0,
+  force: 0,
+  habilite: 0,
+  magie: 0,
+  mouvement: 0,
+  perception: 0,
+  sociabilite: 0,
+  survie: 0,
+  tir: 0,
+  volonte: 0,
+});
   //Jet de dés : méthode
   throw2D10() : number {
     let num1 = Math.floor(Math.random() * 10) + 1;
@@ -12,5 +27,76 @@ export class BrigCharacCreateService {
     return num1 + num2
   }
 
-
+humainStats(){
+this.charac_race.set('Humain')
+this.stats.set({
+  'combat': 25,
+  'connaissances': 25,
+  'discretion': 25,
+  'endurance': 25,
+  'force': 25,
+  'habilite': 25,
+  'magie': 0,
+  'mouvement': 25,
+  'perception': 25,
+  'sociabilite': 25,
+  'survie': 25,
+  'tir': 25,
+  'volonte': 25,
+});
+}
+elfeStats(){
+this.charac_race.set('Elfe')
+this.stats.set({
+  'combat': 25,
+  'connaissances': 25,
+  'discretion': 25,
+  'endurance': 20,
+  'force': 20,
+  'habilite': 25,
+  'magie': 0,
+  'mouvement': 25,
+  'perception': 30,
+  'sociabilite': 25,
+  'survie': 30,
+  'tir': 30,
+  'volonte': 20,
+});
+}
+nainStats(){
+this.charac_race.set('Nain')
+this.stats.set({
+  'combat': 30,
+  'connaissances': 25,
+  'discretion': 25,
+  'endurance': 30,
+  'force': 25,
+  'habilite': 30,
+  'magie': 0,
+  'mouvement': 20,
+  'perception': 20,
+  'sociabilite': 20,
+  'survie': 20,
+  'tir': 20,
+  'volonte': 30,
+});
+}
+halfelinStats(){
+this.charac_race.set('Halfelin')
+this.stats.set({
+  'combat': 20,
+  'connaissances': 25,
+  'discretion': 35,
+  'endurance': 20,
+  'force': 15,
+  'habilite': 30,
+  'magie': 0,
+  'mouvement': 30,
+  'perception': 25,
+  'sociabilite': 35,
+  'survie': 25,
+  'tir': 25,
+  'volonte': 20,
+});
+}
 }

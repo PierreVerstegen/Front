a930b2a1c896f44b8d36204b926db1c67db967b6
Pierre Verstegen <pierrev21@gmail.com>
2025-11-28 14:37:09 +0100

    Added presentation & forms elements

--- Projet/src/component/shared/forms/brig-form-component/brig-form-component.ts
+++ Projet/src/component/shared/forms/brig-form-component/brig-form-component.ts
@@ -5,7 +5,7 @@ import { CommonModule } from '@angular/common';
 import { BrigCharacCreateService } from '../../../../core/service/brig-charac-create-service/brig-charac-create-service';
 @Component({
   selector: 'app-brig-form-component',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './brig-form-component.html',
   styleUrl: './brig-form-component.css',
 })
@@ -16,24 +16,148 @@ characCreateService = inject(BrigCharacCreateService)
 charac_race = this.characCreateService.charac_race
 stats = this.characCreateService.stats
 
+affichage = "+ 2D10"
+isDisabled = false;
+isDisabled2 = false;
+selectedArchetype = this.characCreateService.selectedArchetype
+archetypeDict : {} = {
+                "Aigle":"Ambitieux(-se), sûr(e) de moi, j'ai un fort esprit de compétition. Connaissances ou Perception +5, Volonté +5",
+                "Ane":"Tranquille, altruiste, plaisantin et têtu(e). Endurance +5, Habileté ou Survie +5, Volonté +5, Connaissances -5",
+                "Cerf":"Honorable, droit(e) dans mes bottes, et fier(e). Sociabilité +5, Volonté +5, Survie ou Mouvement +5, Discrétion ou Tir -5",
+                "Chat":"Svelte, charmeur(-se), mystérieux(-se), parfois cruel(-le). Discrétion +5, Perception +5, Mouvement +5, Force ou Volonté -5",
+                "Chien":"Amical(e), loyal(e), courageux (-se), mais aussi impulsif(-ive). Endurance +5, Perception +5, Sociabilité +5, Discrétion ou Habileté -5",
+                "Coq":"Vaniteux(-se), va-t-en-guerre, et séducteur(-trice). Combat +5, Sociabilité +5, Discrétion ou Habileté -5",
+                "Corbeau":"Mystérieux(-se), d’allure sombre, j’ai mauvaise réputation. Connaissances +5, Discrétion ou Survie +5, Volonté +5, Sociabilité -5",
+                "Fourmi":"Plutôt petit(e), travailleur(-se), sérieux(-se). Connaissances ou Discrétion +5, Habileté +5, Volonté +5, Perception ou Sociabilité -5",
+                "Hibou":"Je-sais-tout, travailleur(-se), exigeant(e). Connaissances +10, Habileté ou Perception +5, Sociabilité ou Volonté +5, Combat -5",
+                "Hyène":"Moqueur(-se), cruel(-le), violent(e). Combat +5, Discrétion ou Mouvement+5, Force ou Volonté -5",
+                "Lapin":"Plutôt petit(e), enjoué(e), j’aime plaire. Discrétion +5, Mouvement +5, Perception +5, Sociabilité +10, Combat -5, Force -5",
+                "Lion":"Imposant(e), charismatique, j'ai une forte estime de moi. Combat +5, Force +5, Sociabilité +5, Connaissances ou Habileté -5, Discrétion -5",
+                "Loup":"Patibulaire, violent(e), loyal(e). Combat +5, Perception ou Survie +5, Sociabilité -5",
+                "Mouton":"Suiveur(-se), prudent(e), amical(e). Habileté +5, Perception +5, Sociabilité +5, Volonté -5",
+                "Ours":"Gros(-se) et fort(e), mais aussi protecteur(-trice). Force +5, Endurance +5, Perception ou Survie +5, Connaissances ou Discrétion -5",
+                "Paon":"Frivole, vaniteux(-se), sociabilite. Sociabilité +10, Connaissances ou Perception +5, Force -5",
+                "Porc":"Fort(e), gras(-se), lubrique, j'aime la fête et la bonne chère. Force +5, Endurance +5, Sociabilité +5, Mouvement ou Connaissances -5",
+                "Rat":"Plutôt petit(e), avare, méchant(e). Discrétion +5, Connaissances ou Volonté +5, Endurance ou Survie +5, Force -5",
+                "Renard":"Plutôt svelte, rusé(e), séducteur(-trice). Mouvement +5, Sociabilité +5, Habileté ou Discrétion +5, Endurance ou Force -5",
+                "Serpent":"Froid(e), calculateur(-trice), cynique. Connaissances +5, Sociabilité +5, Volonté +5, Mouvement ou Perception -5",
+                "Singe":"Agile, amical(e), moqueur(-se), plaisantin. Mouvement +10, Sociabilité +5, Volonté -5",
+                "Souris":"Plutôt petit(e), enjoué(e), courageux(-se). Discrétion +10, Habileté +5, Mouvement +5, Sociabilité +5, Combat -5, Force -5",
+                "Taureau":"Gros (-se) et fort(e), violent(e), j’aime m’imposer. Combat +5, Endurance +5, Force +5, Discrétion -5, Connaissances ou Habileté -5",
+                "Vautour":"Dégingandé(e), moqueur(-se), cynique. Connaissances ou Endurance +5, Perception +5, Volonté +5, Mouvement ou Sociabilité -5",
+}
+
+// bidule test
+
+bonusSlider1= this.characCreateService.bonusSlider1;
+bonusSlider2= this.characCreateService.bonusSlider2;
+
+
+
+
+
 // Méthodes appelant service
 
 humainStats(){
-this.characCreateService.humainStats
-console.log(this.stats)
-console.log(this.charac_race)
-}elfeStats(){
-this.characCreateService.elfeStats
+this.characCreateService.humainStats()
+this.isDisabled = false
+}
+elfeStats(){
+this.characCreateService.elfeStats()
+this.isDisabled = false
 }
 nainStats(){
-this.characCreateService.nainStats
+this.characCreateService.nainStats()
+this.isDisabled = false
 }
 halfelinStats(){
-this.characCreateService.halfelinStats
+this.characCreateService.halfelinStats()
+this.isDisabled = false
 }
 
+generateAll(){
+  this.characCreateService.generateAll()
+  this.affichage = ""
+  this.isDisabled = true
 
+}
 
+updateStats(){
 
+  this.characCreateService.updateStats()
+  this.isDisabled2 = true
+}
+
+resetStats(){
+if (this.charac_race() === 'Humain'){
+this.stats.set({
+  'combat': 25,
+  'connaissances': 25,
+  'discretion': 25,
+  'endurance': 25,
+  'force': 25,
+  'habilete': 25,
+  'magie': 0,
+  'mouvement': 25,
+  'perception': 25,
+  'sociabilite': 25,
+  'survie': 25,
+  'tir': 25,
+  'volonte': 25,
+});
+}
+if (this.charac_race() === 'Elfe'){
+this.stats.set({
+  'combat': 25,
+  'connaissances': 25,
+  'discretion': 25,
+  'endurance': 20,
+  'force': 20,
+  'habilete': 25,
+  'magie': 0,
+  'mouvement': 25,
+  'perception': 30,
+  'sociabilite': 25,
+  'survie': 30,
+  'tir': 30,
+  'volonte': 20,})
+}
+if (this.charac_race() === 'Nain'){
+this.stats.set({
+  'combat': 30,
+  'connaissances': 25,
+  'discretion': 25,
+  'endurance': 30,
+  'force': 25,
+  'habilete': 30,
+  'magie': 0,
+  'mouvement': 20,
+  'perception': 20,
+  'sociabilite': 20,
+  'survie': 20,
+  'tir': 20,
+  'volonte': 30,
+});
+}
+if (this.charac_race() === 'Halfelin'){
+this.stats.set({
+  'combat': 20,
+  'connaissances': 25,
+  'discretion': 35,
+  'endurance': 20,
+  'force': 15,
+  'habilete': 30,
+  'magie': 0,
+  'mouvement': 30,
+  'perception': 25,
+  'sociabilite': 35,
+  'survie': 25,
+  'tir': 25,
+  'volonte': 20,});
+}
+this.affichage = "+2D10"
+  this.isDisabled = false
+  this.isDisabled2 = false
+}
 
 }
--- Projet/src/core/service/brig-charac-create-service/brig-charac-create-service.ts
+++ Projet/src/core/service/brig-charac-create-service/brig-charac-create-service.ts
@@ -11,7 +11,7 @@ export class BrigCharacCreateService {
   discretion: 0,
   endurance: 0,
   force: 0,
-  habilite: 0,
+  habilete: 0,
   magie: 0,
   mouvement: 0,
   perception: 0,
@@ -20,12 +20,18 @@ export class BrigCharacCreateService {
   tir: 0,
   volonte: 0,
 });
+  
+  selectedArchetype = signal<string>("");
   //Jet de dés : méthode
-  throw2D10() : number {
+  throw2D10(value : number) : number {
     let num1 = Math.floor(Math.random() * 10) + 1;
     let num2 = Math.floor(Math.random() * 10) + 1;
-    return num1 + num2
+    value = num1 + num2 + value
+    return value
   }
+
+bonusSlider1 = signal<number>(0)
+bonusSlider2 = signal<number>(0)
 
 humainStats(){
 this.charac_race.set('Humain')
@@ -35,7 +41,7 @@ this.stats.set({
   'discretion': 25,
   'endurance': 25,
   'force': 25,
-  'habilite': 25,
+  'habilete': 25,
   'magie': 0,
   'mouvement': 25,
   'perception': 25,
@@ -53,7 +59,7 @@ this.stats.set({
   'discretion': 25,
   'endurance': 20,
   'force': 20,
-  'habilite': 25,
+  'habilete': 25,
   'magie': 0,
   'mouvement': 25,
   'perception': 30,
@@ -71,7 +77,7 @@ this.stats.set({
   'discretion': 25,
   'endurance': 30,
   'force': 25,
-  'habilite': 30,
+  'habilete': 30,
   'magie': 0,
   'mouvement': 20,
   'perception': 20,
@@ -89,7 +95,7 @@ this.stats.set({
   'discretion': 35,
   'endurance': 20,
   'force': 15,
-  'habilite': 30,
+  'habilete': 30,
   'magie': 0,
   'mouvement': 30,
   'perception': 25,
@@ -99,4 +105,349 @@ this.stats.set({
   'volonte': 20,
 });
 }
+
+
+generateAll() {
+  this.stats.update(stats => {
+    const result = { ...stats }; // Les trois points servent à copier les éléments de stats, en gros on copie un élément pour pouvoir l'update avec la copie
+    for (const key in result) {
+      if (key !== 'magie')
+      result[key] = this.throw2D10(result[key]);
+    }
+    return result;
+  });
 }
+
+updateStats() {
+  this.stats.update(stats => {
+    const result = { ...stats }; // Les trois points servent à copier les éléments de stats, en gros on copie un élément pour pouvoir l'update avec la copie
+    // Aigle
+    if (this.selectedArchetype() === "Aigle"){
+      result['volonte'] += 5
+        if (this.bonusSlider1()=== 0){
+          result['connaissances'] += 5
+        }else{
+          result['perception'] += 5
+        }
+    }
+    // Ane
+    if (this.selectedArchetype() === "Ane"){
+      result['endurance'] += 5
+      result['volonte'] += 5
+      result['connaissances'] -= 5
+        if (this.bonusSlider1()=== 0){
+          result['habilete'] += 5
+        }else{
+          result['survie'] += 5
+        }
+    }
+    // Cerf
+    if (this.selectedArchetype() === "Cerf"){
+      result['sociabilite'] += 5
+      result['volonte'] += 5
+
+        if (this.bonusSlider1()=== 0){
+          result['survie'] += 5
+        }else{
+          result['mouvement'] += 5
+        }
+
+        if (this.bonusSlider2()=== 0){
+          result['discretion'] -= 5
+        }else{
+          result['tir'] -= 5
+        }
+    }
+    // Chat
+    if (this.selectedArchetype() === "Chat"){
+      result['discretion'] += 5
+      result['perception'] += 5
+      result['mouvement'] += 5
+        if (this.bonusSlider1()=== 0){
+          result['force'] -= 5
+        }else{
+          result['volonte'] -= 5
+        }
+      }
+      // Chien
+      if (this.selectedArchetype() === "Chien"){
+      result['endurance'] += 5
+      result['perception'] += 5
+      result['sociabilite'] += 5
+        if (this.bonusSlider1()=== 0){
+          result['discretion'] -= 5
+        }else{
+          result['habilete'] -= 5
+        }
+      }
+      // Coq
+      if (this.selectedArchetype() === "Coq"){
+      result['combat'] += 5
+      result['sociabilite'] += 5
+
+        if (this.bonusSlider1()=== 0){
+          result['discretion'] -= 5
+        }else{
+          result['habilete'] -= 5
+        }
+      }
+      // Corbeau
+      if (this.selectedArchetype() === "Corbeau"){
+      result['connaissances'] += 5
+      result['volonte'] += 5
+      result['sociabilite'] -= 5
+        if (this.bonusSlider1()=== 0){
+          result['discretion'] += 5
+        }else{
+          result['survie'] += 5
+        }
+      }
+      // Fourmi
+      if (this.selectedArchetype() === "Fourmi"){
+      result['habilete'] += 5
+      result['volonte'] += 5
+      
+        if (this.bonusSlider1()=== 0){
+          result['connaissances'] += 5
+        }else{
+          result['discretion'] += 5
+        }
+
+        if (this.bonusSlider2()=== 0){
+          result['perception'] -= 5
+        }else{
+          result['sociabilite'] -= 5
+        }
+      }
+
+      // Hibou
+      if (this.selectedArchetype() === "Hibou"){
+      result['connaissances'] += 10
+      result['combat'] -= 5
+      
+        if (this.bonusSlider1()=== 0){
+          result['habilete'] += 5
+        }else{
+          result['perception'] += 5
+        }
+
+        if (this.bonusSlider2()=== 0){
+          result['sociabilite'] += 5
+        }else{
+          result['volonte'] += 5
+        }
+      }
+
+      // Hyène
+      if (this.selectedArchetype() === "Hyène"){
+      result['combat'] += 5
+      
+        if (this.bonusSlider1()=== 0){
+          result['discretion'] += 5
+        }else{
+          result['mouvement'] += 5
+        }
+
+        if (this.bonusSlider2()=== 0){
+          result['force'] -= 5
+        }else{
+          result['volonte'] -= 5
+        }
+      }
+
+      // Lapin
+      if (this.selectedArchetype() === "Lapin"){
+      result['discretion'] += 5
+      result['mouvement'] += 5
+      result['perception'] += 5
+      result['sociabilite'] += 10
+
+        result['force'] -= 5
+        result['combat'] -= 5
+      }
+
+      // Lion
+      if (this.selectedArchetype() === "Lion"){
+      result['combat'] += 5
+      result['force'] += 5
+      result['sociabilite'] += 5
+      result['discretion'] -= 5
+
+      if (this.bonusSlider1()=== 0){
+          result['connaissances'] -= 5
+        }else{
+          result['habilete'] -= 5
+        }
+      }
+
+      // Loup
+      if (this.selectedArchetype() === "Loup"){
+      result['combat'] += 5
+      result['sociabilite'] -= 5
+
+
+      if (this.bonusSlider1()=== 0){
+          result['perception'] += 5
+        }else{
+          result['survie'] += 5
+        }
+      }
+
+      // Mouton
+      if (this.selectedArchetype() === "Mouton"){
+      result['habilete'] += 5
+      result['perception'] += 5
+      result['sociabilite'] += 5
+
+        result['volonte'] -= 5
+      }
+
+      // Ours
+      if (this.selectedArchetype() === "Ours"){
+      result['force'] += 5
+      result['endurance'] += 5
+
+        if (this.bonusSlider1()=== 0){
+          result['perception'] += 5
+        }else{
+          result['survie'] += 5
+        }
+
+        if (this.bonusSlider2()=== 0){
+          result['connaissances'] -= 5
+        }else{
+          result['discretion'] -= 5
+        }
+      }
+
+      // Paon
+      if (this.selectedArchetype() === "Paon"){
+      result['force'] -= 5
+      result['sociabilite'] += 10
+
+
+      if (this.bonusSlider1()=== 0){
+          result['connaissances'] += 5
+        }else{
+          result['perception'] += 5
+        }
+      }
+
+      // Porc
+      if (this.selectedArchetype() === "Ours"){
+      result['force'] += 5
+      result['endurance'] += 5
+      result['sociabilite'] += 5
+
+        if (this.bonusSlider1()=== 0){
+          result['mouvement'] -= 5
+        }else{
+          result['connaissances'] -= 5
+        }
+      }
+
+      // Rat
+      if (this.selectedArchetype() === "Rat"){
+      result['force'] -= 5
+      result['discretion'] += 5
+
+        if (this.bonusSlider1()=== 0){
+          result['connaissances'] += 5
+        }else{
+          result['volonte'] += 5
+        }
+
+        if (this.bonusSlider2()=== 0){
+          result['endurance'] += 5
+        }else{
+          result['survie'] += 5
+        }
+      }
+
+      // Renard
+      if (this.selectedArchetype() === "Renard"){
+      result['mouvement'] += 5
+      result['sociabilite'] += 5
+
+        if (this.bonusSlider1()=== 0){
+          result['habilete'] += 5
+        }else{
+          result['discretion'] += 5
+        }
+
+        if (this.bonusSlider2()=== 0){
+          result['endurance'] -= 5
+        }else{
+          result['force'] -= 5
+        }
+      }
+
+      // Serpent
+      if (this.selectedArchetype() === "Serpent"){
+      result['connaissances'] += 5
+      result['sociabilite'] += 5
+      result['volonte'] += 5
+
+        if (this.bonusSlider1()=== 0){
+          result['mouvement'] -= 5
+        }else{
+          result['perception'] -= 5
+        }
+
+      }
+      // Singe
+      if (this.selectedArchetype() === "Singe"){
+      result['mouvement'] += 10
+      result['sociabilite'] += 5
+      result['volonte'] -= 5
+      }
+
+      // Souris
+      if (this.selectedArchetype() === "Souris"){
+      result['discretion'] += 10
+      result['habilete'] += 5
+      result['mouvement'] += 5
+      result['sociabilite'] += 5
+
+      result['combat'] -= 5
+      result['force'] -= 5
+      }
+
+      // Taureau
+      if (this.selectedArchetype() === "Taureau"){
+      result['force'] += 5
+      result['combat'] += 5
+      result['endurance'] += 5
+      result['discretion'] -= 5
+
+        if (this.bonusSlider1()=== 0){
+          result['connaissances'] -= 5
+        }else{
+          result['habilete'] -= 5
+        }
+
+      }
+      // Vautour
+      if (this.selectedArchetype() === "Vautour"){
+      result['perception'] += 5
+      result['volonte'] += 5
+
+        if (this.bonusSlider1()=== 0){
+          result['connaissances'] += 5
+        }else{
+          result['endurance'] += 5
+        }
+
+        if (this.bonusSlider2()=== 0){
+          result['mouvement'] -= 5
+        }else{
+          result['sociabilite'] -= 5
+        }
+      }
+    return result;
+  });
+}
+
+
+
+} // <----- Closes the service
